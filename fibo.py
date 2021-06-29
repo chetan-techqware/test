1dd993add95891f8173a9d01869fa5007a3cbf66
Chetan Saini <86496655+chetan-techqware@users.noreply.github.com>
2021-06-29 11:10:16 +0530

    Update fibo.py
    Fibonacci series

--- fibo.py
+++ fibo.py
@@ -1,17 +1,17 @@
-# def fibo(num):
-#     a,b = 0,1
-#     mylist = []
-#     for i in range(num):
-#         a,b = b,a+b
-#         mylist.append(a)
-#     return mylist
+#this is the code for fibonnaci series using normal function
+def fibo(num):
+    a,b = 0,1
+    mylist = []
+    for i in range(num):
+        a,b = b,a+b
+        mylist.append(a)
+    return mylist
         
+x = (fibo(20))
+print(x)
 
-    
 
-# x = (fibo(20))
-# print(x)
-
+# This is the code for fibonacci series using generator 
 def fibo_gen(num):
     a,b = 0,1
     for i in range(num):
